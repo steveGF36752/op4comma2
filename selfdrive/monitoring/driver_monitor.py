c377fc0653e910c3910120777d72bca7a824abac
DFyffe <fyffed@netzero.net>
2021-05-20 07:45:11 -0400

    openpilot v0.8.4 release

--- selfdrive/monitoring/driver_monitor.py
+++ selfdrive/monitoring/driver_monitor.py
@@ -17,7 +17,7 @@ EventName = car.CarEvent.EventName
 _AWARENESS_TIME = 35.  # passive wheel touch total timeout
 _AWARENESS_PRE_TIME_TILL_TERMINAL = 12.
 _AWARENESS_PROMPT_TIME_TILL_TERMINAL = 6.
-_DISTRACTED_TIME = 11.
+_DISTRACTED_TIME = 15.
 _DISTRACTED_PRE_TIME_TILL_TERMINAL = 8.
 _DISTRACTED_PROMPT_TIME_TILL_TERMINAL = 6.
 
@@ -50,8 +50,8 @@ _POSE_OFFSET_MAX_COUNT = 3600  # stop deweighting new data after 6 min, aka "sho
 _RECOVERY_FACTOR_MAX = 5.  # relative to minus step change
 _RECOVERY_FACTOR_MIN = 1.25  # relative to minus step change
 
-MAX_TERMINAL_ALERTS = 3  # not allowed to engage after 3 terminal alerts
-MAX_TERMINAL_DURATION = 300  # 30s
+MAX_TERMINAL_ALERTS = 30  # not allowed to engage after 3 terminal alerts
+MAX_TERMINAL_DURATION = 30  # 30s
 
 # model output refers to center of cropped image, so need to apply the x displacement offset
 RESIZED_FOCAL = 320.0
