d080c315488e4101c30ab6950188d776db2226c2
DFyffe <fyffed@netzero.net>
2021-06-11 09:35:27 -0400

    Add files via upload

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,31 +1,5 @@
 #!/usr/bin/bash
-if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
 
-    echo "Installing fonts..."
-
-    mount -o rw,remount /system
-
-  	cp -f /data/openpilot/selfdrive/assets/fonts/opensans_* /system/fonts/
-    cp -f /data/openpilot/selfdrive/assets/fonts.xml /system/etc/fonts.xml
-    chmod 644 /system/etc/fonts.xml
-  	chmod 644 /system/fonts/opensans_*
-    
-    cp /data/openpilot/installer/bootanimation.zip /system/media/
-    mount -o remount,r /system
-    
-    echo =================================================================
-    echo =================================================================
-    echo Reboot Now..!!
-    echo =================================================================
-    reboot
-fi
-
-if [ ! -f "/data/BOOTLOGO" ]; then
-    /usr/bin/touch /data/BOOTLOGO
-    dd if=/data/openpilot/installer/splash.img of=/dev/block/bootdevice/by-name/splash
-    echo =================================================================
-    echo Comma boot logo change complete
-    
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
