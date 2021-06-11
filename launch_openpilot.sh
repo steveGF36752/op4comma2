d26d355994cef4d50adb371a9add73b6d2b90652
DFyffe <fyffed@netzero.net>
2021-06-11 10:47:48 -0400

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -12,20 +12,15 @@ if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
     
     cp /data/openpilot/installer/bootanimation.zip /system/media/
     mount -o remount,r /system
-    
-    setprop persist.sys.locale en-US
-    setprop persist.sys.language en
-    setprop persist.sys.country US
-    setprop persist.sys.timezone America/New_York
-    reboot
-fi
 
 if [ ! -f "/data/BOOTLOGO" ]; then
     /usr/bin/touch /data/BOOTLOGO
     dd if=/data/openpilot/installer/splash.img of=/dev/block/bootdevice/by-name/splash
     echo =================================================================
     echo Comma boot logo change complete
-
+    
+fi
+    
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
