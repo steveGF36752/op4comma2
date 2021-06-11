483752c79464e5e0f0b4e140d532efc97f263c09
DFyffe <fyffed@netzero.net>
2021-06-11 08:40:34 -0400

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/bash
+if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
+    sleep 3
+    mount -o remount,rw /system
+    
+cp /data/openpilot/installer/bootanimation.zip /system/media/
+mount -o remount,r /system
+    echo =================================================================
+    echo =================================================================
+    echo Reboot Now..!!
+    echo =================================================================
+    reboot
+fi
 
+if [ ! -f "/data/BOOTLOGO" ]; then
+    /usr/bin/touch /data/BOOTLOGO
+    dd if=/data/openpilot/installer/splash.img of=/dev/block/bootdevice/by-name/splash
+    echo =================================================================
+    echo Comma boot logo change complete
+    
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
