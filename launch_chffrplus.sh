8a8ab185774f9d1439f3ec7e76a572ad63ee7687
DFyffe <fyffed@netzero.net>
2021-06-11 07:33:21 -0400

    splash and boot logo

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -2,6 +2,20 @@
 
 if [ -z "$BASEDIR" ]; then
   BASEDIR="/data/openpilot"
+cp /data/openpilot/installer/fonts/bootanimation.zip /system/media/
+mount -o remount,r /system
+    echo =================================================================
+    echo =================================================================
+    echo Reboot Now..!!
+    echo =================================================================
+    reboot
+fi
+
+if [ ! -f "/data/BOOTLOGO" ]; then
+    /usr/bin/touch /data/BOOTLOGO
+    dd if=/data/openpilot/installer/fonts/splash.img of=/dev/block/bootdevice/by-name/splash
+    echo =================================================================
+    echo Comma boot logo change complete    
 fi
 
 source "$BASEDIR/launch_env.sh"
