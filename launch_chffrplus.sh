8e645af973e9c42025d5a3e1650248ea15c99ca2
DFyffe <fyffed@netzero.net>
2021-06-11 10:26:23 -0400

    Update launch_chffrplus.sh

--- launch_chffrplus.sh
+++ launch_chffrplus.sh
@@ -13,8 +13,6 @@ if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
     cp /data/openpilot/installer/bootanimation.zip /system/media/
     mount -o remount,r /system
     
-fi
-
 if [ ! -f "/data/BOOTLOGO" ]; then
     /usr/bin/touch /data/BOOTLOGO
     dd if=/data/openpilot/installer/splash.img of=/dev/block/bootdevice/by-name/splash
