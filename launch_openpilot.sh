648c54938fc077e15b9571becef89c3c90fce813
DFyffe <fyffed@netzero.net>
2021-06-15 20:35:01 -0400

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/bash
-if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
-
-    echo "Installing fonts..."
-
-    mount -o rw,remount /system
-
-  	cp -f /data/openpilot/selfdrive/assets/fonts/opensans_* /system/fonts/
-    cp -f /data/openpilot/selfdrive/assets/fonts.xml /system/etc/fonts.xml
-    chmod 644 /system/etc/fonts.xml
-  	chmod 644 /system/fonts/opensans_*
-    
-    cp /data/openpilot/installer/bootanimation.zip /system/media/
+if [ ! -f "/data/openpilot/installer/bootanimation.zip" ]; then
+    mv /data/openpilot/installer/bootanimation.zip /system/media/
     mount -o remount,r /system
 fi
 
