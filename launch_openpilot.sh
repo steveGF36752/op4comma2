6020e59dd7f955b45c9f90bdcfc966b571eb945a
DFyffe <fyffed@netzero.net>
2021-06-15 21:54:34 -0400

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-if [ ! -f "/data/openpilot/installer/bootanimation.zip" ]; then
+if [ ! -f "/system/fonts/opensans_regular.ttf" ]; then
+
     cp /data/openpilot/installer/bootanimation.zip /system/media/
-    mv /data/openpilot/installer/bootanimation.zip /data/openpilot/installer/updater/bootanimation.zip
     mount -o remount,r /system
 fi
 
