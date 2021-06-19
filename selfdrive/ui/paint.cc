1ee4b757f7696a54c8dfd14a796d1448b2da2288
DFyffe <fyffed@netzero.net>
2021-06-19 17:24:40 -0400

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -894,7 +894,7 @@ static void ui_draw_vision_header(UIState *s) {
 
   ui_draw_vision_maxspeed(s);
   ui_draw_vision_speed(s);
-  ui_draw_vision_event(s);
+  //ui_draw_vision_event(s);
   bb_ui_draw_UI(s);
   ui_draw_extras(s);
 }
