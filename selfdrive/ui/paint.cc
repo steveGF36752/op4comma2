f445dee632698fdb4b7d3ddfe531ef1beb5ee812
DFyffe <fyffed@netzero.net>
2021-06-19 23:30:40 -0400

    adjust tpms

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -903,18 +903,18 @@ static void ui_draw_tpms(UIState *s) {
   int viz_tpms_w = 240;
   int viz_tpms_h = 160;
   int viz_tpms_x = s->viz_rect.x + s->viz_rect.w - 270;
-  int viz_tpms_y = s->viz_rect.x + 700;
+  int viz_tpms_y = s->viz_rect.x + 800;
   char tpmsFl[32];
   char tpmsFr[32];
   char tpmsRl[32];
   char tpmsRr[32];
 
-  // Draw Border
+  // Draw TPMS Border
   const Rect rect = {viz_tpms_x, viz_tpms_y, viz_tpms_w, viz_tpms_h};
   ui_draw_rect(s->vg, rect, COLOR_WHITE_ALPHA(80), 5, 20);
   nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BASELINE);
   const int pos_x = viz_tpms_x + (viz_tpms_w / 2);
-  const int pos_y = 655;
+  const int pos_y = 650;
   const int pos_add = 50;
   const int fontsize = 60;
 
