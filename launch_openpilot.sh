427f5239a4cd1c4c3b0387a6b9684e9cbcead301
DFyffe <fyffed@netzero.net>
2021-06-24 21:10:25 -0400

    Add files via upload

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
-
+if [ ! -f "/data/BOOTLOGO" ]; then
+    /usr/bin/touch /data/BOOTLOGO
+    dd if=/data/openpilot/installer/splash.img of=/dev/block/bootdevice/by-name/splash
+    echo =================================================================
+    echo Comma boot logo change complete
+    mount -o rw,remount /system
+    cp /data/openpilot/installer/bootanimation.zip /system/media/
+    mount -o ro,remount /system
+fi  
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
-
