3856317f4d126e2b17e8c6f3517f6c3d9f71ef66
DFyffe <fyffed@netzero.net>
2021-06-26 07:42:51 -0400

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -208,11 +208,13 @@ static void draw_frame(UIState *s) {
 
 static void ui_draw_vision_lane_lines(UIState *s) {
   const UIScene &scene = s->scene;
-  // paint lanelines
-  for (int i = 0; i < std::size(scene.lane_line_vertices); i++) {
-    NVGcolor color = nvgRGBAf(1.0, 1.0, 1.0, scene.lane_line_probs[i]);
-    ui_draw_line(s, scene.lane_line_vertices[i], &color, nullptr);
-  }
+  NVGpaint track_bg;
+  if (!scene.end_to_end) {
+    // paint lanelines
+    for (int i = 0; i < std::size(scene.lane_line_vertices); i++) {
+      NVGcolor color = nvgRGBAf(1.0, 1.0, 1.0, scene.lane_line_probs[i]);
+      ui_draw_line(s, scene.lane_line_vertices[i], &color, nullptr);
+    }
 
     // paint road edges
     for (int i = 0; i < std::size(scene.road_edge_vertices); i++) {
@@ -224,32 +226,6 @@ static void ui_draw_vision_lane_lines(UIState *s) {
   } else {
     track_bg = nvgLinearGradient(s->vg, s->fb_w, s->fb_h, s->fb_w, s->fb_h * .4,
                                           COLOR_RED_ALPHA(180), COLOR_RED_ALPHA(0));
-  }
-	  
-  // paint path
-  int steerOverride = s->scene.car_state.getSteeringPressed();
-  NVGpaint track_bg;
-  if (s->scene.controls_state.getEnabled()) {
-  // Draw colored track
-    if (steerOverride) {
-      track_bg = nvgLinearGradient(s->vg, s->fb_w, s->fb_h, s->fb_w, s->fb_h*.4,
-                                   COLOR_ENGAGEABLE, COLOR_ENGAGEABLE_ALPHA(120));
-    } else if (scene.end_to_end) {
-      track_bg = nvgLinearGradient(s->vg, s->fb_w, s->fb_h, s->fb_w, s->fb_h*.4,
-                                   COLOR_RED, COLOR_RED_ALPHA(120));
-    } else {
-      // color track with output scale
-      int torque_scale = (int)fabs(510*(float)s->scene.output_scale);
-      int red_lvl = fmin(255, torque_scale);
-      int green_lvl = fmin(255, 510-torque_scale);
-      track_bg = nvgLinearGradient(s->vg, s->fb_w, s->fb_h, s->fb_w, s->fb_h*.4,
-        nvgRGBA(          red_lvl,            green_lvl,  0, 255),
-        nvgRGBA((int)(0.5*red_lvl), (int)(0.5*green_lvl), 0, 50));
-    }
-  } else {
-    // Draw white vision track
-    track_bg = nvgLinearGradient(s->vg, s->fb_w, s->fb_h, s->fb_w, s->fb_h*.4,
-                                 COLOR_WHITE, COLOR_WHITE_ALPHA(120));
   }
   // paint path
   ui_draw_line(s, scene.track_vertices, nullptr, &track_bg);
