cdc76f250c75ed4a13b7d2ef0a6cb056c867e483
DFyffe <fyffed@netzero.net>
2021-07-04 08:50:19 -0400

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -102,7 +102,7 @@ class CarController():
 
     # fix for Genesis hard fault at low speed
     if UseSMDPS == True:
-        apply_steer = 0
+        # Smartmdps present
     else:
       min_set_speed = 30 * CV.KPH_TO_MS
       if CS.out.vEgo < 55 * CV.KPH_TO_MS and self.car_fingerprint == CAR.GENESIS and not CS.mdps_bus:
